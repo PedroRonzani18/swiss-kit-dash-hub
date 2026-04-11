2b46430925037bc498ab50d3719810b8f5be5892
Pedro Augusto de Portilho Ronzani <111985618+PedroRonzani18@users.noreply.github.com>
2026-04-11 10:29:06 -0300

    feat(eslint): integrate custom unused vars rule and update eslint configurations (#31)

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { createUnusedVarsRule } from "@swisskit/eslint-config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -20,15 +21,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
-          ignoreRestSiblings: true,
-        },
-      ],
+      "@typescript-eslint/no-unused-vars": createUnusedVarsRule({ ignoreRestSiblings: true }),
     },
   },
   {
